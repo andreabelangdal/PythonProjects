c0e689169373bced67b61055760c2a7d4d5b3904
andrea belangdal <lbelangdal@mail.weber.edu>
2018-11-15 18:57:37 -0700

    Update test.py
    print notes

---
+++ test.py
@@ -0,0 +1,44 @@
+# # print("Hello World")
+# # print('Hello World')
+
+# #print("I can't Do it")
+# print('Scott sure "tries"'
+
+# # escape character
+# print('This is the first line \n This is teh second line')
+# print("\n")
+
+# # printed an integer and an integer string
+# print(35)
+# print("35")
+
+# # Concentration combining strings
+# firstName = "Andrea"                # this is an inline comment
+# lastName  = "Belangdal"             # this stores the user's last name.
+
+# print(firstName + " " + lastName)   # firstName lastName
+
+#     # Other Options
+# print(firstName,lastName)
+
+# print ('{0} first name {1} last name'.format(firstName, lastName))
+# print ('{1} first name {0} last name'.format(firstName, lastName))
+
+# printing integers
+# firstNumber = 5
+# secondNumber = 10
+# print(firstNumber + secondNumber)
+# print(firstNumber, secondNumber)
+# print('{0} is greater than {1}'.format(firstNumber, secondNumber)) 
+
+# preform math function
+# floatingNumber 3.24532
+highTestScore = 0.9327 #floating point number
+lowTestScore  = 0.4598 #floating point number
+print('The high score was ' + str(highTestScore) + '\n the low score was ' + str(lowTestScore))
+print('The high score was {0:.2f}\nThe low score was {1:.2f}'.format(highTestScore, lowTestScore))
+
+print('The print a list of things'
+        'Apple\n'
+        'Bananan\n'
+        'Orange\n')
